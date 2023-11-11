86956b1c6bbfe92df333a093afa2b9e200bad771
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-11-11 22:55:15 +0100

    ajout indicateur victoire + boutons s'affiche ne ordre aleatoire + mokSession pour fausse base de données

--- jeu.php
+++ jeu.php
@@ -1,74 +1,129 @@
 <?php
 include 'header.php';
 
+//Ne pas décommenter sinon détruit $_SESSION 
+// session_destroy();
+
+//Ne pas décommenter sinon supprime la dernière entrée du tableau d'énigmes
+// array_pop($_SESSION["enigmes"]);
+
 // var_dump($_GET['id']);
+
+//Verifie si id existe dans get ou post et le set dans $id
+if (isset($_GET["id"])){
+    $id = $_GET["id"];
+} else if (isset($_POST["id"])) {
+    $id = $_POST["id"];
+};
+//Verifie le niveau de diffulté et set dans $difficulty le mot en francais
+if ($_SESSION["enigmes"][$id]["difficulty"] == "easy"){
+    $difficulty = "FACILE";
+};
+if ($_SESSION["enigmes"][$id]["difficulty"] == "medium"){
+    $difficulty = "NORMAL";
+};
+if ($_SESSION["enigmes"][$id]["difficulty"] == "hard"){
+    $difficulty = "DIFFICILE";
+};
+if ($_SESSION["enigmes"][$id]["difficulty"] == "impossible"){
+    $difficulty = "PÈRE FOURRAS";
+};
+
+$win = $_SESSION["enigmes"][$id]["win"];
 // var_dump($_SESSION);
 // var_dump($_SESSION["enigmes"]);
 // var_dump($_SESSION["enigmes"][$_GET["id"]]["win"]);
 // var_dump($_SESSION["enigmes"][$_GET["id"]]["lose"]);
+// var_dump($_SESSION["enigmes"][$_GET["id"]]["correctResponse"]);
+
+//Si les indicateurs win et lose n'existe pas, les crees à zero pour la page stats.php
+if (!isset($_SESSION["enigmes"][$id]["win"])) {
+    $_SESSION["enigmes"][$id]["win"] = 0;
+};
+if (!isset($_SESSION["enigmes"][$id]["win"])) {
+    $_SESSION["enigmes"][$id]["lose"] = 0;
+};
 
 
-$response1 = "1";
-$response2 = "2";
-$response3 = "3";
-$response4 = "4";
+//Réponses envoyées par les boutons
+$response1 = $_SESSION["enigmes"][$id]["correctResponse"];
+$response2 = $_SESSION["enigmes"][$id]["falseResponse1"];
+$response3 = $_SESSION["enigmes"][$id]["falseResponse2"];
+$response4 = $_SESSION["enigmes"][$id]["falseResponse3"];
 
-//Verifie qu'un id existe dans $_GET et si oui affiche l'énigme correspondante dans $enigmes sinon message d'erreur
-if (isset($_GET["id"])) {
-    $id = $_GET["id"];
+//Couleurs des boutons
+$buttonsColors = ["danger","secondary","primary","success"];
+//Pour changer la couleur des boutons aleatoirement
+shuffle($buttonsColors);
+
+//Boutons pour jouer
+$buttons = ["<a type='button' class='btn btn-$buttonsColors[0] m-2' style='width: 20%;' href='jeu.php?resp=" . $response1 . "&id=" . $id . "'>$response1</a>", 
+            "<a type='button' class='btn btn-$buttonsColors[1] m-2' style='width: 20%;' href='jeu.php?resp=" . $response2 . "&id=" . $id . "'>$response2</a>", 
+            "<a type='button' class='btn btn-$buttonsColors[2] m-2' style='width: 20%;' href='jeu.php?resp=" . $response3 . "&id=" . $id . "'>$response3</a>", 
+            "<a type='button' class='btn btn-$buttonsColors[3] m-2' style='width: 20%;' href='jeu.php?resp=" . $response4 . "&id=" . $id . "'>$response4</a>"];
+//pour changer l'ordre des boutons aléatoirement
+shuffle($buttons);
+
+//Verifie qu'un id existe, si oui affiche l'énigme correspondante depuis $enigmes sinon message d'erreur
+if (isset($id)) {
     $enigme = $_SESSION["enigmes"][$id];
-    $contenu = "<div class='container w-25 d-flex flex-wrap justify-content-around bg-dark text-white mb-4 rounded-5'>
+    $contenu = "<div class='container w-25 d-flex flex-wrap justify-content-around bg-dark text-white my-5 rounded-5'>
                     <h1 class='text-center'>" . $enigme["titre"] . "</h1>
                 </div>
-                <div class='container d-flex flex-wrap justify-content-around bg-dark text-white mb-4 rounded-5'>
-                    <p class='container-fluid d-flex w-50 p-3 m-auto justify-content-center align-items-center' style='height: 200px;'>" . $enigme["description"] . "</p>
-                    <div class='container-fluid d-flex justify-content-around align-items-center' style='height: 100px;'>
-                        <p>Reponse A : " . $enigme["falseResponse2"] . "</p>
-                        <p>Reponse B : " . $enigme["falseResponse1"] . "</p>
-                        <p>Reponse C : " . $enigme["correctResponse"] . "</p>
-                        <p>Reponse D : " . $enigme["falseResponse3"] . "</p>
+                <div class='container w-25 d-flex flex-wrap justify-content-around mb-4'>
+                    <h4 class='text-center'>Niveau de difficulté: " . $difficulty . "</h4>
+                </div>
+                <div class='container w-25 d-flex flex-wrap justify-content-around mb-4'>
+                    <p class='text-center'>Victoires: " . $win . " fois</p>
+                </div>
+                <div class='container d-flex flex-wrap flex-column justify-content-around bg-dark text-white mb-4 rounded-5'>
+                    <div class='container-fluid d-flex w-50 p-3 m-auto justify-content-center align-items-center' style='height: 200px;''>
+                        <p class='m-auto'>" . $enigme["description"] . "</p>
+                    </div>
+                    <div class='container-fluid d-flex w-25 m-auto justify-content-center align-items-center bg-light text-dark mb-4 rounded-5'>
+                        <p class='m-auto fs-4 fw-bold'>Choisissez une Réponse :</p>
                     </div>
                 </div>
-                <div class='container d-flex w-50 flex-wrap justify-content-around  bg-dark text-white mb-4 rounded-5'>
-                    <a type='button' class='btn btn-primary m-2' href='jeu.php?resp=". $response3 . "&id=$id'>Réponse A</a>
-                    <a type='button' class='btn btn-secondary m-2' href='jeu.php?resp=". $response2 . "&id=$id'>Réponse B</a>
-                    <a type='button' class='btn btn-danger m-2' href='jeu.php?resp=". $response1 . "&id=$id'>Réponse C</a>
-                    <a type='button' class='btn btn-success m-2' href='jeu.php?resp=". $response4 . "&id=$id'>Réponse D</a>
+                <div class='container d-flex w-50 flex-wrap justify-content-around bg-dark text-white mb-4 rounded-5'>
+                    $buttons[0]
+                    $buttons[1]
+                    $buttons[2]
+                    $buttons[3]
                 </div>";
 } else {
     $msgError = "<p>L'énigme n'a pas été trouvée.</p>";
 };
- 
-//verifie la réponse, si oui ajoute 1 a win ou lose pour les stats et affiche un message
-if (isset($_GET["resp"]) && $_GET["resp"] == "1") {
-    $cle = $_GET["id"];
-    if (isset($_SESSION["enigmes"][$cle]["win"])) {
-        $_SESSION["enigmes"][$cle]["win"] ++ ;
+
+//verifie la réponse, affiche un message bravo et 1 a win si gagné sinon un message pour perdu et ajoute 1 a lose
+if (isset($_GET["resp"]) && $_GET["resp"] == $response1) {
+    //verifie que win existe et incremente de 1 sinon le cree à 1
+    if (isset($_SESSION["enigmes"][$id]["win"])) {
+        $_SESSION["enigmes"][$id]["win"] ++ ;
     } else {
-        $_SESSION["enigmes"][$cle]["win"] = 1;
+        $_SESSION["enigmes"][$id]["win"] = 1;
     };
     $msgSuccess = "<h1 class='text-center'>BRAVO</h1>
                   <div class='container d-flex flex-wrap justify-content-around text-white mb-4'>
                     <a type='button' class='btn btn-light m-2' href='list.php'>Retour aux énigmes</a>
                   </div>";
-} else if (isset($_GET["resp"]) && $_GET["resp"] !== "1") {
-    $cle = $_GET["id"];
-    if (isset($_SESSION["enigmes"][$cle]["lose"])) {
-        $_SESSION["enigmes"][$cle]["lose"] ++ ;
+} else if (isset($_GET["resp"]) && $_GET["resp"] !== $response1) {
+     //verifie que lose existe et incremente de 1 sinon le cree à 1
+    if (isset($_SESSION["enigmes"][$id]["lose"])) {
+        $_SESSION["enigmes"][$id]["lose"] ++ ;
     } else {
-        $_SESSION["enigmes"][$cle]["lose"] = 1;
+        $_SESSION["enigmes"][$id]["lose"] = 1;
     };
     $msgError = "<h1 class='text-center'>PERDU</h1>
                  <div class='container d-flex flex-wrap justify-content-around text-white mb-4'>
                      <a type='button' class='btn btn-info m-2' href='jeu.php?id=" . $id . "'>Rejouer</a>
                      <a type='button' class='btn btn-light m-2' href='list.php'>Retour aux énigmes</a>
                  </div>";
-    
-} else {};
+} else {
+};
 ?>
 
 <main class="">
-    
+
     <?php
     //Verifie qu'il y a bien une enigme dans contenu et l'affiche sinon affiche un message d'erreur
     if (!empty($contenu)) {
@@ -82,7 +137,7 @@ if (isset($_GET["resp"]) && $_GET["resp"] == "1") {
         echo "</div>";
     }
     ?>
-    
+
 </main>
 
 <?php
---
+++ mokSession.php
@@ -0,0 +1,47 @@
+<?php
+$_SESSION["enigmes"] = [ '0' => [
+                            'titre' => 'test1',
+                            'description' => 'blablabla le Magicien 1',
+                            'difficulty' => 'easy',
+                            'falseResponse1' => 'Merlin',
+                            'falseResponse2' => 'Fourras',
+                            'falseResponse3' => 'lol',
+                            'correctResponse' => 'moi',
+                            'lose' => 0, 
+                            'win' => 0
+                            ],  
+                         '1' => [
+                            'titre' => 'test2',
+                            'description' => 'blablabla le Magicien 2',
+                            'difficulty' => 'medium',
+                            'falseResponse1' => 'Merlin',
+                            'falseResponse2' => 'Fourras',
+                            'falseResponse3' => 'lol',
+                            'correctResponse' => 'moi',
+                            'lose' => 0, 
+                            'win' => 0
+                            ],
+                         '2' => [
+                            'titre' => 'test3',
+                            'description' => 'blablabla le Magicien 3',
+                            'difficulty' => 'hard',
+                            'falseResponse1' => 'Merlin',
+                            'falseResponse2' => 'Fourras',
+                            'falseResponse3' => 'lol',
+                            'correctResponse' => 'moi',
+                            'lose' => 0, 
+                            'win' => 0
+                            ],
+                         '3' => [
+                            'titre' => 'test4',
+                            'description' => 'blablabla le Magicien 4',
+                            'difficulty' => 'impossible',
+                            'falseResponse1' => 'Merlin',
+                            'falseResponse2' => 'Fourras',
+                            'falseResponse3' => 'lol',
+                            'correctResponse' => 'moi',
+                            'lose' => 0, 
+                            'win' => 0
+                            ],
+                        ];
+?>
