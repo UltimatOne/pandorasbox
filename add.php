03f15ed3d8733eaa66c142919a9f3cc3266dd269
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-01-03 11:58:07 +0100

    mise en forme page add

--- add.php
+++ add.php
@@ -16,30 +16,32 @@ include 'services/getUsersFromBdd.php';
   <?php
   if (!empty($msgSuccess) or !empty($msgError)) {
     echo "<div class='bg-dark bg-opacity-50 d-flex flex-column align-items-center' style='position: absolute; z-index: 10; top: 0; bottom: 0; left: 0; right: 0;'>
-                <div class='w-25 mt-5'>";
+                <div class='w-25 position-fixed bg-white d-flex flex-column align-items-center p-2 rounded-5 border border-2 border-black' style='top: 45vh;'>";
     include "components/box.php";
-    echo "</div>
-    <a href='add.php' class='btn btn-success '>Terminé</a>
+    echo "<a href='add.php' class='btn btn-success '>Terminé</a></div>
             </div>";
   }
   ?>
   <section class="d-flex flex-column align-items-center " style='width: 45%;'>
     <h1 class="text-center">Énigmes</h1>
-    <!-- $key = le numéro d'index (pas l'id) -->
-    <?php foreach ($enigmes as $key => $enigme) { ?>
-      <div class='row bg-dark text-white justify-content-between py-1 gap-1 w-75 '>
-        <p class='col'>
-          <?= $enigme['enigme_title'] ?>
-        </p>
-        <p class='col'>
-          <?= substr($enigme['enigme_description'], 0, 45) ?>...
-        </p>
-        <div class="d-flex justify-content-evenly ">
-          <a href="add.php?modEnigme=<?= $key ?>" class=' btn btn-success' style="width:40%">Modifier</a>
-          <a href="add.php?suppEnigme=<?= $enigme['enigme_id'] ?>" class=' btn btn-danger' style="width:40%">Supprimer</a>
+    <div class="d-flex flex-column align-items-center overflow-auto " style="height: 45vh">
+      <!-- $key = le numéro d'index (pas l'id) -->
+      <?php foreach ($enigmes as $key => $enigme) { ?>
+        <div class='row bg-dark text-white justify-content-between py-1 gap-1 w-75 '>
+          <p class='col'>
+            <?= $enigme['enigme_title'] ?>
+          </p>
+          <p class='col'>
+            <?= substr($enigme['enigme_description'], 0, 45) ?>...
+          </p>
+          <div class="d-flex justify-content-evenly ">
+            <a href="add.php?modEnigme=<?= $key ?>" class=' btn btn-success' style="width:40%">Modifier</a>
+            <a href="add.php?suppEnigme=<?= $enigme['enigme_id'] ?>" class=' btn btn-danger' style="width:40%">Supprimer</a>
+          </div>
         </div>
-      </div>
-    <?php }
+      <?php } ?>
+    </div>
+    <?php
     if (isset($_GET['modEnigme'])) {
       include "components/adminFormModifEnigmes.php";
     } else {
@@ -49,23 +51,26 @@ include 'services/getUsersFromBdd.php';
   </section>
   <section class="d-flex flex-column align-items-center " style='width: 45%;'>
     <h1 class="text-center">Utilisateurs</h1>
-    <?php foreach ($users as $key => $user) { ?>
-      <div class='row bg-dark text-white justify-content-between py-1 gap-1 w-75 '>
-        <p class='col'>
-          <?= $user['user_pseudo'] ?>
-        </p>
-        <p class='col'>
-          <?= $user['user_email'] ?>
-        </p>
-        <p class='col'>
-          <?= $user['user_role'] ?>
-        </p>
-        <div class="d-flex justify-content-evenly ">
-          <a href="add.php?modUser=<?= $user['user_id'] ?>" class='btn btn-success ' style="width:40%">Modifier</a>
-          <a href="add.php?suppUser=<?= $user['user_id'] ?>" class='btn btn-danger ' style="width:40%">Supprimer</a>
+    <div class="d-flex flex-column align-items-center overflow-auto " style="height: 45vh">
+      <?php foreach ($users as $key => $user) { ?>
+        <div class='row bg-dark text-white justify-content-between py-1 gap-1 w-75 '>
+          <p class='col'>
+            <?= $user['user_pseudo'] ?>
+          </p>
+          <p class='col'>
+            <?= $user['user_email'] ?>
+          </p>
+          <p class='col'>
+            <?= $user['user_role'] ?>
+          </p>
+          <div class="d-flex justify-content-evenly ">
+            <a href="add.php?modUser=<?= $user['user_id'] ?>" class='btn btn-success ' style="width:40%">Modifier</a>
+            <a href="add.php?suppUser=<?= $user['user_id'] ?>" class='btn btn-danger ' style="width:40%">Supprimer</a>
+          </div>
         </div>
-      </div>
-    <?php }
+      <?php } ?>
+    </div>
+    <?php
     include "components/adminFormAddUsers.php";
     ?>
   </section>
