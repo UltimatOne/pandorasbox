a78f6c797fef13e95523e99d457103461866e3f3
Thibault Breguet <thibault.breguet18@gmail.com>
2024-01-03 11:15:42 +0100

    Finalisation de la modification des énigmes

--- add.php
+++ add.php
@@ -2,6 +2,7 @@
 include "components/header.php";
 include "services/connexionUserCheck.php";
 include 'services/addEnigmeIntoBdd.php';
+include 'services/modifEnigmeIntoBdd.php';
 include 'services/userCreateValid.php';
 include 'services/getCorrectResponsesSelectFromBdd.php';
 include 'services/deleteUserToBdd.php';
@@ -34,59 +35,17 @@ include 'services/getUsersFromBdd.php';
           <?= substr($enigme['enigme_description'], 0, 45) ?>...
         </p>
         <div class="d-flex justify-content-evenly ">
-          <a href="add.php?mod=<?= $enigme['enigme_id'] ?>" class=' btn btn-success' style="width:40%">Modifier</a>
+          <a href="add.php?modEnigme=<?= $key ?>" class=' btn btn-success' style="width:40%">Modifier</a>
           <a href="add.php?suppEnigme=<?= $enigme['enigme_id'] ?>" class=' btn btn-danger' style="width:40%">Supprimer</a>
         </div>
       </div>
-    <?php } ?>
-    <h2>Ajouter une énigme</h2>
-    <form class="container w-50 bg-dark text-center mt-5 rounded-5 py-2 text-white fw-bold mb-2" action=""
-      method="post">
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_title">Titre de l'énigme</label>
-        <input type="text" name="enigme_title">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_description" class="form-label">Énoncé de l'énigme</label>
-        <textarea name="enigme_description" cols="80" rows="10"></textarea>
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_difficulty">Niveau de difficulté</label>
-        <select class="form-control" name="enigme_difficulty">
-          <option value="">-----------</option>
-          <option value="easy">Facile</option>
-          <option value="medium">Moyen</option>
-          <option value="hard">Difficile</option>
-          <option value="impossible">Père Fouras</option>
-        </select>
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_response1">réponse 1</label>
-        <input type="text" name="enigme_response1">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_response2">réponse 2</label>
-        <input type="text" name="enigme_response2">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_response3">réponse 3</label>
-        <input type="text" name="enigme_response3">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_response4">réponse 4</label>
-        <input type="text" name="enigme_response4">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="enigme_correctResponse">réponse correcte:</label>
-        <select class="form-control" name="enigme_correctResponse">
-          <option value="">---------</option>
-          <?php foreach ($correctResponsesSelect as $correctResponseSelect) {
-            echo "<option value='{$correctResponseSelect['correctResponse']}'>{$correctResponseSelect['correctResponse']}</option>";
-          } ?>
-        </select>
-      </div>
-      <button type="submit" class="btn btn-dark my-2">Ajouter l'énigme</button>
-    </form>
+    <?php }
+    if (isset($_GET['modEnigme'])) {
+      include "components/adminFormModifEnigmes.php";
+    } else {
+      include "components/adminFormAddEnigmes.php";
+    }
+    ?>
   </section>
   <section class="d-flex flex-column align-items-center " style='width: 45%;'>
     <h1 class="text-center">Utilisateurs</h1>
@@ -102,48 +61,13 @@ include 'services/getUsersFromBdd.php';
           <?= $user['user_role'] ?>
         </p>
         <div class="d-flex justify-content-evenly ">
-          <a href="add.php?mod=<?= $user['user_id'] ?>" class='btn btn-success ' style="width:40%">Modifier</a>
+          <a href="add.php?modUser=<?= $user['user_id'] ?>" class='btn btn-success ' style="width:40%">Modifier</a>
           <a href="add.php?suppUser=<?= $user['user_id'] ?>" class='btn btn-danger ' style="width:40%">Supprimer</a>
         </div>
       </div>
-    <?php } ?>
-    <h2>Ajouter un utilisateur</h2>
-    <form action="" method="post"
-      class="container w-50 bg-dark text-center mt-5 rounded-5 py-2 text-white fw-bold mb-2">
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_role">Rôle</label>
-        <select class="form-control" name="user_role">
-          <option value="">-----------</option>
-          <option value="administrator">Administrateur</option>
-          <option value="player">Joueur</option>
-        </select>
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_firstname" class="form-label">Prénom</label>
-        <input type="text" class="form-control" name="user_firstname">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_lastname" class="form-label">Nom</label>
-        <input type="text" class="form-control" name="user_lastname">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_pseudo" class="form-label">Pseudo</label>
-        <input type="text" class="form-control" name="user_pseudo">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_email" class="form-label">Email</label>
-        <input type="email" class="form-control" name="user_email">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_pswrd" class="form-label">mot de passe</label>
-        <input type="password" class="form-control" name="user_pswrd">
-      </div>
-      <div class="d-flex flex-column w-75 mx-auto">
-        <label for="user_birthday" class="form-label">date de naissance</label>
-        <input type="date" class="form-control" name="user_birthday">
-      </div>
-      <button type="submit" class="btn btn-dark w-25 my-2">Envoyer</button>
-    </form>
+    <?php }
+    include "components/adminFormAddUsers.php";
+    ?>
   </section>
 </main>
 <?php
